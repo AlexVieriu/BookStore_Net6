9113bfef30100c7f46eacb17073ebbd115364df3
Vieriu Alexandru <vieriu.alexandru.25@gmail.com>
2023-03-25 13:27:38 +0200

    Update to .net 7

--- BookStore.UI.Server/GlobalUsingUI.cs
+++ BookStore.UI.Server/GlobalUsingUI.cs
@@ -1,16 +1,12 @@
-﻿global using Microsoft.AspNetCore.Components.Authorization;
-global using Blazored.LocalStorage;
-global using System.IdentityModel.Tokens.Jwt;
-
-global using BookStore.UI.Server.Providers;
-global using BookStore.UI.Server.Services.Base;
-global using BookStore.UI.Server.Services.User;
-
-global using BookStore.UI.Server.MappingUI;
-global using BookStore.UI.Server.Services.Author;
-global using BookStore.UI.Server.Services.Book;
-
+﻿global using System.IdentityModel.Tokens.Jwt;
 global using System.Net.Http.Headers;
-global using BookStore.UI.Server.Static;
-
+global using Blazored.LocalStorage;
+global using BookStore.UI.Server.MappingUI;
 global using BookStore.UI.Server.Models;
+global using BookStore.UI.Server.Providers;
+global using BookStore.UI.Server.Services.Author;
+global using BookStore.UI.Server.Services.Base;
+global using BookStore.UI.Server.Services.Book;
+global using BookStore.UI.Server.Services.User;
+global using BookStore.UI.Server.Static;
+global using Microsoft.AspNetCore.Components.Authorization;
